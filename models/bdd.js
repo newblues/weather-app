6d10567394003cee107560669aae40ae1cd46135
Antoine Sauvage <new_blues@hotmail.com>
2019-05-14 09:21:14 +0200

    update database

--- models/bdd.js
+++ models/bdd.js
@@ -1,27 +1,33 @@
 // Do not forget to store your new request module in a variable in order to use it
 const mongoose = require('mongoose');
 
-var user = "capsule";
-var password = "azerty1";
-var port = 24778;
-var bddname = "weatherapp";
+var user = 'master';
+var password = 'master123';
+var port = 13599;
+var bddname = 'weatherapp';
 
-
-// useNewUrlParser ;)
 var options = {
-   connectTimeoutMS: 5000,
-   useNewUrlParser: true,
-  };
+  connectTimeoutMS: 5000,
+  useNewUrlParser: true
+};
 
-mongoose.connect('mongodb://'+user+':'+password+'@ds024778.mlab.com:'+port+'/'+bddname,
-    options,
-    function(err) {
-     if (err) {
-       console.log(err);
-     } else {
-       console.info('connexion ok');
-     }
+mongoose.connect(
+  'mongodb://' +
+    user +
+    ':' +
+    password +
+    '@ds013599.mlab.com:' +
+    port +
+    '/' +
+    bddname,
+  options,
+  function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.info('connexion ok');
     }
+  }
 );
 
 module.exports = mongoose;
